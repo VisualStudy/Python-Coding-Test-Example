db72ef4d3c1b838eba62b457b6dd3cbe500b7b8d
VisualStudy <sparksoldier@outlook.com>
2026-05-21 21:42:09 +0900

    create caclulate_height function

--- Python-Coding-Test-Example/PCCE/Science/water_rocket.py
+++ Python-Coding-Test-Example/PCCE/Science/water_rocket.py
@@ -1,1 +1,12 @@
 ﻿# 물로켓 최대 높이 계산 예제
+
+def calculate_height(initial_velocity):
+    '''
+    initital_velocity: 초기 속도, 단위 m/s
+    return: 최대 높이, 단위 m
+    '''
+
+    g = 9.8 # 중력가속도 m/s^2
+    height = (initial_velocity ** 2) / (2 * g)
+
+    return height
